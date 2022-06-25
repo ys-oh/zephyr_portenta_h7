710f49dd5e085dcd3721bddb4c8b12650f6662e5
ys-oh <oyster90@naver.com>
2022-06-25 22:56:43 +0900

    initial commit for blink test

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+#include <zephyr.h>
+#include <kernel.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <drivers/led.h>
+
+void main(void)
+{
+    const struct device* dev = DEVICE_DT_GET(DT_NODELABEL(gpioled)); 
+    if (!device_is_ready(dev))
+    {
+        LOG_ERR("gpioled is not ready.");
+        return;
+    }
+
+    while (1)
+    {
+        LOG_INF("led on\n");
+        led_on(dev, 0);
+        led_on(dev, 1);
+        led_on(dev, 2);
+        k_sleep(K_MSEC(500));
+
+        LOG_INF("led off\n");
+        led_off(dev, 0);
+        led_off(dev, 1);
+        led_off(dev, 2);
+        k_sleep(K_MSEC(500));
+    }
+}
